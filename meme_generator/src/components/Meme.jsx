93acce3546ba89194fd6cfeaaca3f9b8c249eb6a
Bill Nguyen Ton Hoang <bill.nguyentonhoang@live.com>
2023-07-12 15:34:41 +0100

    GenerateMemes functionality implemented

--- meme_generator/src/components/Meme.jsx
+++ meme_generator/src/components/Meme.jsx
@@ -1,25 +1,37 @@
 import React from 'react'
 import "./styles/Meme.css"
+import memesData from "../memes"
 
-function Meme() 
-{
-    function generateRandomMemes() {
-        const memesArray = memesData.data.memes
-        
-        const min = 0
-        const max = memesArray.length
-        let random = Math.floor(min + Math.random() * (max - 1 - min));
-        
-        console.log(memesArray[random])
-    }
+function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
     
+    
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input className="form--input" placeholder="Top text" type="text" />
                 <input className="form--input" placeholder="Bottom text" type="text" />
-                <button className="form--button">Get a new meme image 🖼</button>
-            </form>
+                <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
+            </div>
+            <div>
+                <img src={meme.randomImage} className="meme--image" />
+            </div>
         </main>
     )
 }
